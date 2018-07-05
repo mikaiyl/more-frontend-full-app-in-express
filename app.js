279d54187f065a66deda5d03dc7ee41ab8396f74
Mikaiyl <mikaiyl.davis@icloud.com>
2018-07-05 15:14:19 -0400

    finished mostly

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const path = require("path")
+const express = require('express')
+const path = require('path')
 
 const app = express()
-const publicFolderPath = path.join(__dirname, "public")
+const publicFolderPath = path.join(__dirname, 'public')
 
 app.use(express.json())
 app.use(express.static(publicFolderPath))
@@ -10,5 +10,21 @@ app.use(express.static(publicFolderPath))
 const users = []
 
 // add POST request listener here
+app.post( '/api/user', ( req, res ) => {
 
-app.listen(3000);
+    if ( !users.includes( req.body.username ) ) {
+        let user = req.body.username
+        users.push( user )
+        console.log( user + ' added to users.\n' )
+        console.log( 'Current users are: ' + users.join(', ') )
+
+        res.status(201).send({ message: 'User ' + user + ' added', user: req.body })
+    } else if ( users.includes( req.body.username ) ) {
+        res.status(409).send({ message: 'Username already taken' })
+    } else {
+        res.status(404).send({ message: 'Oops, we can\'t find what you\'re looking for.' })
+    }
+
+} )
+
+app.listen(3000)
--- public/index.js
+++ public/index.js
@@ -1,2 +1,54 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+
+const modal = document.getElementById('modal')
+
+const username = document.getElementById('inputUsername')
+const email = document.getElementById('inputEmail')
+const password = document.getElementById('inputPassword')
+const phone = document.getElementById('inputPhone')
+
+userCreateForm.addEventListener( 'submit', handleClick )
+
+function postData(url, data) {
+    let head = {
+        'Content-Type': 'application/json'
+    }
+    // Default options are marked with *
+    const raw =  fetch( url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: head,
+        body: JSON.stringify(data), // must match 'Content-Type' header
+    }).then( async res => {
+        let obj = {
+            status: res.status,
+            json: await res.json()
+        }
+
+        return obj
+        // parses response to JSON
+    }).then( data => {
+        console.log(data)
+        let { status, json } = data
+        console.log( status )
+        console.log( json )
+        if ( status === 201 ) {
+            document.querySelector( '#modalLabel' ).textContent = json.message
+            $( '#modal' ).modal( 'show' )
+        } else if ( status === 409 ) {
+            document.querySelector( '#modalLabel' ).textContent = json.message
+            $( '#modal' ).modal( 'show' )
+        }
+    } )
+}
+
+function handleClick( event ) {
+    event.preventDefault()
+    let userPackage = {
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        phone: phone.value,
+    }
+    postData( 'http://localhost:3000/api/user', userPackage )
+}
